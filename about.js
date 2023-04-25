629c518ffd6210e200d0b314692538d98fbe96ff
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-04-25 20:35:35 +0200

    Separate about from home page

---
+++ about.js
@@ -0,0 +1,13 @@
+const openMenu = document.getElementById('nav-menu');
+
+function clickX() {
+  openMenu.style.display = 'none';
+}
+
+const ham = document.querySelector('.ham');
+ham.addEventListener('click', () => {
+  openMenu.style.display = 'block';
+});
+
+const closeBtn = document.getElementById('btn-close');
+closeBtn.addEventListener('click', clickX);
